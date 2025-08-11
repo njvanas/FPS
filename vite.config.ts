35fd60c2f4cb5986791fffef1eac2922b3c9e54c
Dolfie <35562093+njvanas@users.noreply.github.com>
2025-08-11 23:01:33 +0200

    Fix GitHub Pages blank screen issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fps-shooter-game/',
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    minify: 'esbuild',
+    minify: 'terser',
     rollupOptions: {
       output: {
         manualChunks: {
